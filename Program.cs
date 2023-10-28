7755c90f531c7dd8cae7efd57de791f7b2ff534f
M3hmetCiftci <Mehmet.cift27@gmail.com>
2023-10-28 21:16:16 +0300

    Console.App-int

---
+++ Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_int
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi;
+            sayi = 30;
+            Console.Write(sayi);
+            Console.ReadLine();
+
+            //Plus(Toplama)
+            //int Number1,Number2;
+            //Number1 = 24;
+            //Number2 = 25;
+            //Console.Write(Number1 + Number2);
+            //Console.Read();
+
+            // 2.choice Plus(Toplama)
+            //int Number1, Number2, Plus;
+            //Number1 = 25;
+            //Number2 = 30;
+            //Plus = Number1 + Number2;
+            //Console.Write(Plus);
+            //Console.Read();
+
+
+
+
+
+
+            // Minus(Çıkarma)
+            //int Number1, Number2;
+            //Number1 = 13;
+            //Number2 = 10;
+            //Console.WriteLine(Number1 - Number2);
+            //Console.Read();
+
+            // 2.choice Minus(Çıkarma)
+            //int Number1, Number2, Minus;
+            //Number1 = 25;
+            //Number2 = 30;
+            //Minus = Number1 - Number2;
+            //Console.Write(Minus);
+            //Console.Read();
+
+
+
+
+
+            //Multiply(Çarpma)
+            //int Number1, Number2;
+            //Number1 = 13;
+            //Number2 = 10;
+            //Console.WriteLine(Number1 * Number2);
+            //Console.Read();
+
+            // 2.choice Multiply(Çarpma)
+            //int Number1, Number2, multiply;
+            //Number1 = 25;
+            //Number2 = 30;
+            //multiply = Number1 * Number2;
+            //Console.Write(multiply);
+            //Console.Read();
+
+
+
+
+            //Divide(Bölme)
+            //int Number1, Number2;
+            //Number1 = 15;
+            //Number2 = 3;
+            //Console.WriteLine(Number1 / Number2);
+            //Console.Read();
+
+            //2.choice Divide(Bölme)
+            //int Number1, Number2, Divide;
+            //Number1 = 25;
+            //Number2 = 30;
+            //Divide = Number1 / Number2;
+            //Console.Write(Divide);
+            //Console.Read();
+
+
+
+            //Plus Minus Multiply Divide
+            //int Number1, Number2,Plus,Minus,Multiply,Divide;
+            //Number1 = 20;
+            //Number2 = 5;
+            //Plus=Number1 + Number2;
+            //Minus=Number1 - Number2;
+            //Multiply=Number1 * Number2;
+            //Divide=Number1 / Number2;
+            //Console.WriteLine("Plus :"+ Plus);
+            //Console.WriteLine("Minus :"+ Minus);
+            //Console.WriteLine("Multiply: "+ Multiply);
+            //Console.WriteLine("Divide :"+ Divide);
+            //Console.Read();
+
+
+        }
+    }
+}
